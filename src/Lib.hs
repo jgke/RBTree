bc5b73b66731b4590cff6ceae08a1dc4fdc8a803
Jaakko Hannikainen <jaakko.hannikainen@solita.fi>
2018-08-10 15:13:04 +0300

    Progress

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,7 @@
 module Lib where
 
-import Data.Maybe (fromJust)
-import Debug.Trace
+import Data.Maybe (fromJust, isJust)
+import Data.Functor ((<&>))
 
 data Color = Black | Red
              deriving (Eq, Show)
@@ -9,16 +9,7 @@ data RBTree a = Nil
               | Node Color a (RBTree a) (RBTree a)
               deriving (Eq, Show)
 
-instance Foldable RBTree where
-   foldMap _ Nil = mempty
-   foldMap f (Node _ k l r) = foldMap f l `mappend` f k `mappend` foldMap f r
-
-instance Ord a => Semigroup (RBTree a) where
-    (<>) = mappend
-
-instance Ord a => Monoid (RBTree a) where
-    mempty = Nil
-    mappend = addAll
+-- basic query functions
 
 makeTree :: a -> RBTree a
 makeTree a = Node Black a Nil Nil
@@ -26,7 +17,69 @@ makeTree a = Node Black a Nil Nil
 makeRed :: a -> RBTree a
 makeRed a = Node Red a Nil Nil
 
+search :: (Ord a) => RBTree a -> a -> Maybe a
+search Nil _ = Nothing
+search (Node _ v l r) needle = case (compare needle v) of
+    LT -> search l needle
+    EQ -> Just v
+    GT -> search r needle
+
+exists :: (Ord a) => RBTree a -> a -> Bool
+exists haystack needle = isJust $ search haystack needle
+
+getNodeValue :: RBTree a -> Maybe a
+getNodeValue Nil = Nothing
+getNodeValue (Node _ val _ _) = Just val
+
+getLeft :: RBTree a -> Maybe (RBTree a)
+getLeft Nil = Nothing
+getLeft (Node _ _ l _) = Just l
+
+getRight :: RBTree a -> Maybe (RBTree a)
+getRight Nil = Nothing
+getRight (Node _ _ _ r) = Just r
+
+-- simple iterating
+
+mapNode :: (a -> RBTree a -> RBTree a -> b) -> RBTree a -> b
+mapNode _ Nil = error "Cannot map a NIL"
+mapNode f (Node _ v l r) = f v l r
+
+walkTree :: (Ord b) => RBTree a -> (a -> RBTree b) -> RBTree b
+walkTree Nil _ = Nil
+walkTree node f = mapNode (\v l r -> flatten [f v, walkTree l f, walkTree r f]) node
+
+anyTree :: (RBTree a -> Bool) -> RBTree a -> Bool
+anyTree _ Nil = False
+anyTree f node@(Node _ _ l r) = f node || anyTree f l || anyTree f r
+
+allTree :: (RBTree a -> Bool) -> RBTree a -> Bool
+allTree f tree = not $ anyTree (not . f) tree
+
+flatten :: (Foldable t, Ord a) => t (RBTree a) -> RBTree a
+flatten treeSet = foldr addAll Nil treeSet
+
+-- color manipulation
+
+isBlack :: RBTree a -> Bool
+isBlack (Node Black _ _ _) = True
+isBlack Nil = True
+isBlack _ = False
+
+isRed :: RBTree a -> Bool
+isRed (Node Red _ _ _) = True
+isRed _ = False
+
+toBlack :: RBTree a -> RBTree a
+toBlack (Node _ x l r) = Node Black x l r
+toBlack Nil = Nil
+
+toRed :: RBTree a -> RBTree a
+toRed (Node _ x l r) = Node Red x l r
+toRed Nil = Nil
+
 -- zipper
+
 type Zipper a = (Thread a, RBTree a)
 data Branch a = RBLeft Color a (RBTree a)
               | RBRight Color a (RBTree a)
@@ -36,9 +89,10 @@ type Thread a = [Branch a]
 zipper :: RBTree a -> Zipper a
 zipper t = ([], t)
 
-unZip :: Zipper a -> RBTree a
-unZip ([], t) = t
-unZip z = unZip (fromJust $ back z)
+unzipper :: Zipper a -> RBTree a
+unzipper ([], t) = t
+unzipper (RBLeft c x r:t, l) = unzipper (t, Node c x l r)
+unzipper (RBRight c x l:t, r) = unzipper (t, Node c x l r)
 
 takeLeft :: Zipper a -> Maybe (Zipper a)
 takeLeft (t, Node c v l r) = Just (RBLeft c v r:t, l)
@@ -53,108 +107,78 @@ back ([], _) = Nothing
 back (RBLeft c x r:t, l) = Just (t, Node c x l r)
 back (RBRight c x l:t, r) = Just (t, Node c x l r)
 
-add :: (Ord a) => a -> RBTree a -> RBTree a
-add a Nil = makeTree a
-add a tree = unZip $ addZ a $ zipper tree
+current :: Zipper a -> RBTree a
+current (_, node) = node
 
-getValue :: RBTree a -> Maybe a
-getValue Nil = Nothing
-getValue (Node _ val _ _) = Just val
+sibling :: Zipper a -> Maybe (RBTree a)
+sibling ([], _) = Nothing
+sibling (RBLeft _ _ r:_, _) = Just r
+sibling (RBRight _ _ l:_, _) = Just l
 
-sibling :: Zipper a -> RBTree a
-sibling ([], _) = error "No sibling without parents"
-sibling (RBLeft _ _ r:_, _) = r
-sibling (RBRight _ _ l:_, _) = l
-
-toBlack :: RBTree a -> RBTree a
-toBlack (Node _ x l r) = Node Black x l r
-toBlack Nil = Nil
-
-
-toRed :: RBTree a -> RBTree a
-toRed (Node _ x l r) = Node Red x l r
-toRed Nil = Nil
-
-currentToRedAndChildrenToBlack :: Zipper a -> Zipper a
-currentToRedAndChildrenToBlack (_, Nil) = error "No children for NIL"
-currentToRedAndChildrenToBlack (t, Node _ v l r) = (t, Node Red v (toBlack l) (toBlack r))
-
-handleRedUncle :: (Ord a) => Zipper a -> Zipper a
-handleRedUncle child = postAddRotation $ currentToRedAndChildrenToBlack grandparent
-  where grandparent = fromJust $ (Just child >>= back >>= back)
+-- modifying zippers
 
 rotateLeft :: Zipper a -> Maybe (Zipper a)
 rotateLeft (t, Node c v l (Node rc rv rl rr)) = Just (t, Node rc rv (Node c v l rl) rr)
-rotateLeft _ = error ""
+rotateLeft _ = Nothing
 
 rotateRight :: Zipper a -> Maybe (Zipper a)
 rotateRight (t, Node c v (Node lc lv ll lr) r) = Just (t, Node lc lv ll (Node c v lr r))
-rotateRight _ = error ""
+rotateRight _ = Nothing
 
-handleBlackUncle :: (Ord a) => Zipper a -> Zipper a
-handleBlackUncle (((RBLeft pc pa pn):(RBLeft gpc gpa gcn):t), child) =
-  fromJust $ Just ((RBLeft gpc pa pn):(RBLeft pc gpa gcn):t, child) >>= back >>= back >>= rotateRight
-handleBlackUncle z@(((RBRight _ _ _):(RBLeft _ _ _):_), _) =
-  handleBlackUncle $ fromJust $ Just z >>= back >>= rotateLeft >>= takeLeft
-handleBlackUncle (((RBRight pc pa pn):(RBRight gpc gpa gcn):t), child) =
-  fromJust $ Just ((RBRight gpc pa pn):(RBRight pc gpa gcn):t, child) >>= back >>= back >>= rotateLeft
-handleBlackUncle z@(((RBLeft _ _ _):(RBRight _ _ _):_), _) =
-  handleBlackUncle $ fromJust $ Just z >>= back >>= rotateRight >>= takeRight
-handleBlackUncle _ = error "No black uncle or inconsistent state"
+swapColorWithParent :: Zipper a -> Maybe (Zipper a)
+swapColorWithParent (((RBLeft pc pa pn):t), (Node c x l r)) = Just (((RBLeft c pa pn):t), (Node pc x l r))
+swapColorWithParent (((RBRight pc pa pn):t), (Node c x l r)) = Just (((RBRight c pa pn):t), (Node pc x l r))
+swapColorWithParent _ = Nothing
 
-postAddRotation :: (Ord a) => Zipper a -> Zipper a
-postAddRotation z@(t, n) = case (back z) of
-  Nothing -> (t, toBlack n)
-  Just parent -> if (isBlack $ snd parent)
-    then z
-    else if (isBlack $ sibling parent)
-         then handleBlackUncle z
-         else handleRedUncle z
+currentToRedAndChildrenToBlack :: Zipper a -> Maybe (Zipper a)
+currentToRedAndChildrenToBlack (_, Nil) = Nothing
+currentToRedAndChildrenToBlack (t, Node _ v l r) = Just (t, Node Red v (toBlack l) (toBlack r))
 
-addZ :: (Ord a) => a -> Zipper a -> Zipper a
-addZ a z@(thread, tree) =
-  let val = getValue tree
-  in case val of
-    Nothing -> postAddRotation (thread, makeRed a)
-    (Just value) -> if (a < value)
-      then addZ a (fromJust $ takeLeft z)
-      else addZ a (fromJust $ takeRight z)
+-- adding
 
-getLeft :: RBTree a -> Maybe (RBTree a)
-getLeft Nil = Nothing
-getLeft (Node _ _ l _) = Just l
-
-getRight :: RBTree a -> Maybe (RBTree a)
-getRight Nil = Nothing
-getRight (Node _ _ _ r) = Just r
+add :: (Ord a) => a -> RBTree a -> RBTree a
+add a Nil = makeTree a
+add a tree = unzipper $ addToZipper a $ zipper tree
 
 addAll :: (Ord a, Foldable t) => RBTree a -> t a -> RBTree a
 addAll tree coll = foldr add tree coll
 
-isBlack :: RBTree a -> Bool
-isBlack (Node Black _ _ _) = True
-isBlack Nil = True
-isBlack _ = False
+handleRedUncle :: (Ord a) => Zipper a -> Maybe (Zipper a)
+handleRedUncle child = Just child >>= back >>= back >>= currentToRedAndChildrenToBlack >>= postAddRotation
 
-isRed :: RBTree a -> Bool
-isRed (Node Red _ _ _) = True
-isRed _ = False
+handleBlackUncle :: (Ord a) => Zipper a -> Maybe (Zipper a)
+handleBlackUncle z@(RBLeft{}:RBLeft{}:_, _) = Just z >>= back >>= swapColorWithParent >>= back >>= rotateRight
+handleBlackUncle z@(RBRight{}:RBRight{}:_, _) = Just z >>= back >>= swapColorWithParent >>= back >>= rotateLeft
+handleBlackUncle z@(RBLeft{}:RBRight{}:_, _) = Just z >>= back >>= rotateRight >>= takeRight >>= handleBlackUncle
+handleBlackUncle z@(RBRight{}:RBLeft{}:_, _) = Just z >>= back >>= rotateLeft >>= takeLeft >>= handleBlackUncle
+handleBlackUncle _ = Nothing
 
-mapNode :: (a -> RBTree a -> RBTree a -> b) -> RBTree a -> b
-mapNode f Nil = error "Cannot map a NIL"
-mapNode f (Node _ v l r) = f v l r
+postAddRotation :: (Ord a) => Zipper a -> Maybe (Zipper a)
+postAddRotation z@(t, n) = case (back z <&> current, back z >>= sibling) of
+  (Nothing, _) -> Just (t, toBlack n)
+  (Just (Node Black _ _ _), _) -> Just z
+  (Just _, Just (Node Black _ _ _)) -> handleBlackUncle z
+  (Just _, Just Nil) -> handleBlackUncle z
+  (Just _, Just (Node Red _ _ _)) -> handleRedUncle z
+  _ -> error ""
 
--- >>= with Ord
-walkTree :: (Ord b) => RBTree a -> (a -> RBTree b) -> RBTree b
-walkTree Nil _ = Nil
-walkTree node f = mapNode (\v l r -> flatten [f v, walkTree l f, walkTree r f]) node
+addToZipper :: (Ord a) => a -> Zipper a -> Zipper a
+addToZipper value z@(thread, tree) =
+  case getNodeValue tree >>= Just . compare value of
+    Nothing -> fromJust $ postAddRotation (thread, makeRed value)
+    (Just GT) -> addToZipper value (fromJust $ takeRight z)
+    (Just EQ) -> addToZipper value (fromJust $ takeLeft z)
+    (Just LT) -> addToZipper value (fromJust $ takeLeft z)
 
-anyTree :: (RBTree a -> Bool) -> RBTree a -> Bool
-anyTree _ Nil = False
-anyTree f node@(Node _ v l r) = f node || anyTree f l || anyTree f r
+-- instances
 
-allTree :: (RBTree a -> Bool) -> RBTree a -> Bool
-allTree f tree = not $ anyTree (not . f) tree
+instance Foldable RBTree where
+   foldMap _ Nil = mempty
+   foldMap f (Node _ k l r) = foldMap f l `mappend` f k `mappend` foldMap f r
 
-flatten :: (Foldable t, Ord a) => t (RBTree a) -> RBTree a
-flatten treeSet = foldr addAll Nil treeSet
+instance Ord a => Semigroup (RBTree a) where
+    (<>) = mappend
+
+instance Ord a => Monoid (RBTree a) where
+    mempty = Nil
+    mappend = addAll
--- test/Spec.hs
+++ test/Spec.hs
@@ -9,20 +9,26 @@ import Test.Tasty.HUnit
 import Lib
 
 import Data.List
-import Data.Ord
 import Data.Foldable
 import Control.Monad
+import Data.Maybe (isJust)
 
+tests :: TestTree
 tests = testGroup "Tests" [properties, unitTests]
 
+main :: IO ()
 main = defaultMain tests
 
 properties :: TestTree
 properties = testGroup "Properties" $ [ scProps, qcProps ]
 
+scProps :: TestTree
 scProps = testGroup "(checked by SmallCheck)" $ makeTests SC.testProperty
-qcProps = testGroup "(checked by QuickCheck)" $ makeTests SC.testProperty
 
+qcProps :: TestTree
+qcProps = testGroup "(checked by QuickCheck)" $ makeTests QC.testProperty
+
+makeTests :: ([Char] -> ([Int] -> Bool) -> a) -> [a]
 makeTests tester = [ prop tester | prop <- propertyTesters ]
 
 treesAreSorted :: Tester
@@ -30,7 +36,7 @@ treesAreSorted a =  a "All trees are sorted" $ \val -> toList (addAll Nil val) =
 
 makeTreeAIsBlack :: Tester
 makeTreeAIsBlack a = a "makeTree a is black" $ \val ->
-  length val /= 1 || (makeTree (head val) :: RBTree Int) == Node Black (head val) Nil Nil
+  (length val /= 1) || (makeTree (head val) :: RBTree Int) == Node Black (head val) Nil Nil
 
 rootOfTreeIsAlwaysBlack :: Tester
 rootOfTreeIsAlwaysBlack a = a "Root of tree is always black" $ \val -> isBlack $ makeTree val
@@ -46,12 +52,11 @@ noAdjacentRedNodes :: Tester
 noAdjacentRedNodes a = a "There are no adjacent red nodes" $ \val -> allTree (not . childAndParentIsRed) $ addAll Nil val
 
 countBlackChildren :: RBTree a -> Int
-countBlackChildren Nil = 0
-countBlackChildren (Node Black _ l r) = 1 + (countBlackChildren l) + (countBlackChildren r)
-countBlackChildren (Node Red _ l r) = 0 + (countBlackChildren l) + (countBlackChildren r)
+countBlackChildren Nil = 1
+countBlackChildren (Node _ _ l r) = max (countBlackChildren l) (countBlackChildren r)
 
 countOfBlackChildrenIsEqual :: Tester
-countOfBlackChildrenIsEqual a = a "Count of children is always equal" $ \val -> allTree (\x -> liftM countBlackChildren (getLeft x) == liftM  countBlackChildren (getRight x)) $ addAll Nil val
+countOfBlackChildrenIsEqual a = a "Count of black children is always equal" $ \val -> allTree (\x -> liftM countBlackChildren (getLeft x) == liftM  countBlackChildren (getRight x)) $ addAll Nil val
 
 zipperLawsLeft :: (Ord a) => RBTree a -> Bool
 zipperLawsLeft tree = ((Just . zipper) tree >>= takeLeft >>= back >>= (Just . snd)) == Just tree
@@ -67,25 +72,44 @@ zipperLeft a = a "takeLeft . back = id" $ \val -> val == [] || zipperLawsLeft (a
 zipperRight :: Tester
 zipperRight a = a "takeRight . back = id" $ \val -> val == [] || zipperLawsRight (addAll Nil (val :: [Int]))
 
+searchSome :: [Int] -> Bool
+searchSome (x:xs) = (not $ elem x xs) || (isJust $ search (addAll Nil xs) x)
+searchSome _ = error ""
 
+searchWorks :: Tester
+searchWorks a = a "Searching tree" $ \val -> val == [] || searchSome val
+
+propertyTesters :: [([Char] -> ([Int] -> Bool) -> t) -> t]
 propertyTesters = [treesAreSorted, makeTreeAIsBlack, rootOfTreeIsAlwaysBlack,
                    noAdjacentRedNodes, countOfBlackChildrenIsEqual,
-                   zipperLeft, zipperRight]
+                   zipperLeft, zipperRight, searchWorks]
 
+makeIntTree :: Int -> RBTree Int
+makeIntTree = makeTree
+
+unitTests :: TestTree
 unitTests = testGroup "Unit tests"
   [ testCase "Simple tree" $
-      makeTree "foo" @?= Node Black "foo" Nil Nil
+      makeIntTree 1 @?= Node Black 1 Nil Nil
     , testCase "Insert to tree" $
-      add "bar" (makeTree "foo") @?= Node Black "foo" (Node Red "bar" Nil Nil) Nil
-    , testCase "Insert more to tree" $
-      add "bar" (makeTree "foo") @?= Node Black "foo" (Node Red "bar" Nil Nil) Nil
-
+      add 2 (makeIntTree 1) @?= Node Black 1 Nil (Node Red 2 Nil Nil)
     , testCase "Navigating empty tree" $
       ((Just $ zipper (Nil :: RBTree Int)) >>= takeLeft) @?= Nothing
     , testCase "Navigating one tree" $
-      ((Just $ zipper (makeTree 'a')) >>= takeLeft >>= (Just . snd)) @?= Just Nil
+      ((Just $ zipper (makeIntTree 0)) >>= takeLeft >>= (Just . snd)) @?= Just Nil
     , testCase "Navigating back and forth" $
-      ((Just $ zipper (makeTree 'a')) >>= takeLeft >>= back >>= (Just . snd)) @?= Just (makeTree 'a')
-    --, testCase "Insert many more to tree" $
-    -- addAll (makeTree 4) [1, 2, 3, 4, 5] @?= Black 4 (Black 3 (Black 2 (Black 1 Nil Nil) Nil) Nil) (Black 5 (Black 4 Nil Nil) Nil)
+      ((Just $ zipper (makeIntTree 0)) >>= takeLeft >>= back >>= (Just . snd)) @?= Just (makeTree 0)
+    , testCase "Insert many more to tree" $
+      addAll (makeIntTree 4) [1, 2, 3, 4, 5] @?= Node Black 4 (Node Red 3 (Node Black 2 (Node Red 1 Nil Nil) Nil) (Node Black 4 Nil Nil)) (Node Black 5 Nil Nil)
+    , testCase "Search from Nil" $
+      search Nil "foo" @?= Nothing
+    , testCase "Search from simple tree" $
+      search (makeIntTree 1) 1 @?= Just 1
+    , testCase "Search from simple tree" $
+      search (makeIntTree 0) 1 @?= Nothing
+    , testCase "Search from larger tree" $
+      search (addAll (makeIntTree 4) [1, 2, 3, 4, 5]) 1 @?= Just 1
+    , testCase "Search from larger tree" $
+      search (addAll (makeIntTree 4) [1, 2, 3, 4, 5]) 0 @?= Nothing
+
   ]
